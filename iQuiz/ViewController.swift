7284af23bebc0896b00544d89936441bef86c9f9
Emnet Gossaye <emnetg@uw.edu>
2015-11-04 00:28:06 -0800

    final version

--- iQuiz/ViewController.swift
+++ iQuiz/ViewController.swift
@@ -9,17 +9,64 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    private var quizzes = ["Mathematics", "Marvel Super Heroes", "Science"]
+    
+    let cellIdentifier = "CellIdentifier"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func settings(sender: UIBarButtonItem) {
+        let alertController = UIAlertController(title: "Settings", message: "Settings go here", preferredStyle: .Alert)
+        
+        let okayAction = UIAlertAction(title: "Okay", style: .Cancel, handler: nil)
+        
+        alertController.addAction(okayAction)
+        self.presentViewController(alertController, animated: true, completion: nil)
+    }
+    
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return quizzes.count
+    }
 
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        var cell = tableView.dequeueReusableCellWithIdentifier(cellIdentifier) as UITableViewCell!
+        
+        if cell == nil {
+            cell = UITableViewCell(style: .Default, reuseIdentifier: cellIdentifier)
+        }
+        
+        cell!.textLabel?.text = quizzes[indexPath.row]
+    
+        var image: UIImage!
+        
+        if cell.textLabel?.text == quizzes[0] {
+            image = UIImage(named: "function")
+            cell!.detailTextLabel?.text = "Do Mathy Stuff!"
+        } else if cell!.textLabel?.text == quizzes[1] {
+            image = UIImage(named: "cape")
+            cell!.detailTextLabel?.text = "Gotham Needs You!"
+        } else {
+            image = UIImage(named: "science")
+            cell!.detailTextLabel?.text = "Do it for Science!"
+
+        }
+        cell!.imageView?.image = image
+        
+        cell.textLabel?.font = UIFont.boldSystemFontOfSize(18)
+        cell.detailTextLabel?.font = UIFont.boldSystemFontOfSize(12)
+        return cell!
+    }
+    
 }
 
